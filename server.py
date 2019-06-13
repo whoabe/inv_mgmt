bcc49d26fa7e918d8dac95237cc0aa3091f30ca6
Abraham Ho <whoabe@Abrahams-MacBook-Air.local>
2019-06-13 16:18:29 +0800

    added 'add store function'

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import peeweedbevolve
 from flask import Flask, render_template, request, flash, redirect, url_for
-from models import db, Store
+from models import db, Store, Warehouse
 import os
 
 app = Flask(__name__)
@@ -39,6 +39,27 @@ def create():
         return redirect(url_for('store'))
     else:
         return render_template('store.html', name=request.args['name]'])
+
+
+
+@app.route("/warehouse")
+def warehouse():
+    stores  = Store.select() #passing stores from the database into the site
+    return render_template('warehouse.html', stores=stores)
+    #render_template renders warehouse.html and sets the stores variable to stores in the html
+
+
+@app.route("/warehouse_form", methods = ["POST"])
+def w_create():
+    w = Warehouse(location=request.form['location'], store = request.form['store_choice'])
+    #Warehouse is the class
+    #location for Warehouse class is from html(name='location') from form
+
+    if w.save():
+        flash("flash saved")
+        return redirect(url_for('warehouse'))
+    else:
+        return render_template('warehouse.html', name=request.args['location]'])        
 
 if __name__ == '__main__':
    app.run()
